a4ea634c736bfd47656499a9c6cc47df3ead40da
TOLU-MICH <michealtolu2003@gmail.com>
2024-07-13 19:10:25 +0100

    edit the aos duration

--- src/component/FeatureCard.tsx
+++ src/component/FeatureCard.tsx
@@ -3,7 +3,7 @@ const FeatureCard = ({ data }: any) => {
     <div
       className="sm:w-[49%] lg:w-[32.333%] p-8 text-center gap-6 flex flex-col shadow-lg cursor-pointer hover:-translate-y-3 transition-transform duration-300"
       data-aos="fade-up"
-      data-aos-duration="800"
+      data-aos-duration="300"
     >
       <div className="mx-auto w-fit">{data.icon}</div>
       <h3 className="font-bold text-xl text-gray-900">{data.title}</h3>
